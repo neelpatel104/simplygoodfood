63d36efac190a183d0ba1c2d96e6d820e68bb94b
zli48 <zli48@ncsu.edu>
2021-11-29 15:25:54 -0500

    login testing

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -68,9 +68,10 @@ export default class Login extends React.Component {
         this.setState({ seller: true });
       }
       this.setState({ loggedIn: true })
-      
+      console.log("logged in");
     })
     .catch(err => {
+      console.log(err);
       throw new Error(err);
     })
   }
@@ -108,13 +109,13 @@ export default class Login extends React.Component {
   async login(event) {
     event.preventDefault();
 
-    try {
+    // try {
       let user = {email: this.state.email, password: this.state.password };
       await this.loginUser(user);
 
-    } catch (err) {
-      alert("Invalid credentials");
-    }
+    // } catch (err) {
+      // alert("Invalid credentials");
+    // }
 
   }
 
