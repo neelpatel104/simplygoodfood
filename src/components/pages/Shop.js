18dc7b47d46b188658f04b9aec11f634c558c877
zli48 <zli48@ncsu.edu>
2021-11-29 17:32:02 -0500

    testing token cookie in requests

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -18,18 +18,18 @@ export default class Shop extends React.Component {
       cart = this.props.location.state.cart;
     }
     
-    let token = document.cookie.split('; ').find(row => row.startsWith('token'));
+    // let token = document.cookie.split('; ').find(row => row.startsWith('token'));
 
-    console.log("cookie " + document.cookie);
-    console.log("token " + token);
-    alert("cookie " + document.cookie);
-    if (token === undefined) {
-      user = "";
-    } else {
+    // console.log("cookie " + document.cookie);
+    // console.log("token " + token);
+    // alert("cookie " + document.cookie);
+    // if (token === undefined) {
+    //   user = "";
+    // } else {
 
-      alert("token " + token);
-      token = token.split('=')[1];
-    }
+    //   alert("token " + token);
+    //   token = token.split('=')[1];
+    // }
 
     this.state = {
       user: user,
@@ -44,7 +44,7 @@ export default class Shop extends React.Component {
       newAmount: "",
       newPickup: "",
       newDelivery: "",
-      token: token,
+      // token: token,
       message: "",
       checkout: false,
       logout: false,
@@ -64,10 +64,10 @@ export default class Shop extends React.Component {
   async getFoodItems(){
     await fetch('https://simplygoodfoodapi.herokuapp.com/foodItems', {
       method: 'GET',
-      credentials: 'include',
-      headers: {
-        'authorization': `Bearer ${this.state.token}`,
-      },
+      credentials: 'true',
+      // headers: {
+      //   'authorization': `Bearer ${this.state.token}`,
+      // },
     })
     .then(response => response.json())
     .then(responseJSON => {
@@ -115,10 +115,10 @@ export default class Shop extends React.Component {
   async deleteFood(name) {
     await fetch(`https://simplygoodfoodapi.herokuapp.com/foodItems/${name}`, {
       method: 'DELETE',
-      credentials: 'include',
-      headers: {
-        'Authorization': `Bearer ${this.state.token}`,
-      },
+      credentials: 'true',
+      // headers: {
+      //   'Authorization': `Bearer ${this.state.token}`,
+      // },
     })
     .then(() => this.getFoodItems())
     .catch(err => {
